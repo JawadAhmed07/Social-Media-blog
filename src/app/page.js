7b887c1bef70b21a6b3635e46d64d7a261a88d99
Jawad Ahmed <qwertykey41@gmail.com>
2024-11-06 23:27:16 +0500

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1>hello testing</h1>
+  );
+}
+
+// <Image
+// className="dark:invert"
+// src="/first.png"
+// alt="Next.js logo"
+// width={180}
+// height={38}
+// priority
+// />
